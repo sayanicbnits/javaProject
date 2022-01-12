89619e73bd9a6f496c1afb1ca8b4fc6603376cfb
deb151292 <debojyoti.chakraborty@cbnits.com>
2022-01-12 10:39:33 +0530

    database program

---
+++ adda/src/main/java/com/example/adda/AddaApplication.java
@@ -0,0 +1,13 @@
+package com.example.adda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddaApplication.class, args);
+	}
+
+}
