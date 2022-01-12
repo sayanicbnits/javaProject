caf6318da50e345454e02987179f5474bc2ce909
unknown <sayani.das@cbnits.com>
2022-01-12 11:05:20 +0530

    sayani

---
+++ First/src/main/java/com/example/demo/ServicePackage/ServiceClass.java
@@ -0,0 +1,14 @@
+package com.example.demo.ServicePackage;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceClass {
+
+	public int result() {
+		int a=5,b=5;
+		return a*b;
+	}
+	
+
+}
--- First/src/main/java/com/example/demo/controller/ControllerClass.java
+++ First/src/main/java/com/example/demo/controller/ControllerClass.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.ServicePackage.ServiceClass;
+
 @RestController
 public class ControllerClass {
+	@Autowired
+	ServiceClass serv;
 	@GetMapping("/hlw")
 	public String hello()
 	{
@@ -36,6 +41,11 @@ public class ControllerClass {
 	public String mos()
 	{
  		return "Hathi mera sathi";
+	}
+	@GetMapping("/result")
+	public int result()
+	{
+ 		return this.serv.result();
 	}
 }
 
