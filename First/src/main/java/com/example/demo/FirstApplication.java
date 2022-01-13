8b609be2152bfd92af71361d79d7b6683dd5e41d
Ankit112001 <ankit.ghosh@cbnits.com>
2022-01-13 16:43:31 +0530

    Ankit

--- First/src/main/java/com/example/demo/FirstApplication.java
+++ First/src/main/java/com/example/demo/FirstApplication.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.callpass.CallPass;
+
 @SpringBootApplication
 public class FirstApplication {
 
